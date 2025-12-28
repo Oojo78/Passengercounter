cf12a919a8d7d4f0de6b42c2cf2fbf8e6c122faf
Oluyemi Ojo <oluyemiojo80@yahoo.co.uk>
2025-12-28 21:00:03 +0000

    This is my second Repository

---
+++ index.js
@@ -0,0 +1,18 @@
+let savEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    savEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+
+
